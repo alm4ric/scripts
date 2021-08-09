0507cdb43334b88513215f3d3c88645a9f8a110d
alm4ric <alm4ric@protonmail.com>
2021-08-09 17:29:46 +0200

    updated

--- bash/nslookup.sh
+++ bash/nslookup.sh
@@ -6,5 +6,5 @@ for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done >
 # remove multiple emty lines and cleanup
 cat -s output-nslookup-$1.tmp > output-nslookup-$1.txt && rm output-nslookup-$1.tmp
 
-# create input list for further (port)scanning (only in-scope object)
+# create input list for further (port)scanning (only in-scope objects)
 grep "$1" output-nslookup-$1.txt > output-nslookup-$1-domainnames.txt
