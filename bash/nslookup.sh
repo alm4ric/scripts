f89c792f11e42dad2cedb24a00c3241892f7d4a5
alm4ric <alm4ric@protonmail.com>
2021-08-09 16:50:21 +0200

    updated

--- bash/nslookup.sh
+++ bash/nslookup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output-nslookup-$1.txt
+# first user argument is the domain input list
+for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output-nslookup-$1.tmp
+
+# remove multiple emty lines and cleanup
+cat -s output-nslookup-$1.tmp > output-nslookup-$1.txt && rm output-nslookup-$1.tmp
