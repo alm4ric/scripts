f8a2f116c41d6dc60d1d158ad5f508e361413ae3
alm4ric <alm4ric@protonmail.com>
2021-08-09 16:34:47 +0200

    updated

--- bash/nslookup.sh
+++ bash/nslookup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-for i in `cat ./input.txt`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output.txt
+for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output-nslookup-$1.txt
