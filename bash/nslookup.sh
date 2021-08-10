25e1f2fd273b24e452d13279a80630565741ebe9
alm4ric <alm4ric@protonmail.com>
2021-08-10 10:29:19 +0200

    updated

--- bash/nslookup.sh
+++ bash/nslookup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# first user argument is the domain input list in format "target.com"
-for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output-nslookup-$1.tmp
+# run nslookup where the first user argument is the domain input list in format "target.com"
+for i in `cat $1`; do nslookup $i | grep ^Name -A1| awk '{print $2}';echo;done > output-nslookup-$1.tmp1
 
-# remove multiple emty lines and cleanup
-cat -s output-nslookup-$1.tmp > output-nslookup-$1.txt && rm output-nslookup-$1.tmp
+# remove empty lines and hostnames that resolved to 127.0.0.1
+sed -n '/127.0.0.1/{s/.*//;x;d;};x;p;${x;p;}' output-nslookup-$1.tmp1 | sed '/^$/d' > output-nslookup-$1.tmp2
 
 # create input list for further (port)scanning (only in-scope objects)
-grep "$1" output-nslookup-$1.txt > output-nslookup-$1-domainnames.txt
+grep "$1" output-nslookup-$1.tmp2 > output-nslookup-$1.txt
+
+# cleanup
+rm output-nslookup-$1.tmp*
